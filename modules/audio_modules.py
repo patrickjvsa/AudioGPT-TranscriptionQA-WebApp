762687b2a20d5d20a9281b2a7d56c97c6a7ea38a
Patrick Joan <138414954+patrickjvsa@users.noreply.github.com>
2023-11-05 21:15:55 -0300

    updated

--- modules/audio_modules.py
+++ modules/audio_modules.py
@@ -6,7 +6,23 @@ Autor: Patrick Vásquez <pvasquezs@fen.uchile.cl>
 Ultima actualización: 05/11/2023
 """
 
+# Import modules
+
 from abc import ABC, abstractmethod
+
+# Format Extracter
+class ExtractDocumentFormat:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def extract_extension(file_path):
+        """
+        Extrae la extensión del archivo. La api de openai soporta m4a, mp3, mp4, mpeg, mpga, wav y webm.
+        :return: La extensión del archivo.
+        """
+        import os
+        name, extension = os.path.splitext(file_path)
+        return extension
 
 # Audio transcriber
 
@@ -21,8 +37,10 @@ class OpenAITranscribeAudio(TranscribeAudio):
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def transcribe_audio(self, file_path):
+    def transcribe_audio(self, file_path, model="whisper-1"):
         import openai
         file = open(file_path, "rb")
-        transcription = openai.Audio.transcribe("whisper-1", file)
+        transcription = openai.Audio.transcribe(model, file)
         return transcription
+
+# Future updates: chunk audio files
