13ff1fcbb701cf77627eb11c562df057a8c6193a
Patrick Joan <138414954+patrickjvsa@users.noreply.github.com>
2023-11-05 22:19:12 -0300

    now working!

--- app.py
+++ app.py
@@ -12,35 +12,50 @@ def clear_history():
 
 if __name__ == "__main__":
 
-    st.subheader('audioGPT by patrick🤖')
-    load_dotenv()
-    api_key = os.getenv('OPENAI_API_KEY')
-
-    with st.sidebar:
-
+    st.write('# audioGPT by patrick🤖🎙️')
+    st.write('\n\n\n\n')
+    col1,col2 = st.columns(2)
+    with col2:
+        st.image('https://dallery.gallery/wp-content/uploads/2022/07/DALL%C2%B7E-prompt-resources-tools-inspiration-for-AI-art-585x775.jpg'
+                 , width=325)
+    with col1:
+        st.write('Añade tu API Key de OpenAI y sube un archivo de audio para comenzar.')
+        # cargar api key desde el archivo .env
+        load_dotenv()
+        api_key = os.getenv('OPENAI_API_KEY')
         # text_input para la api_key
-        api_key = st.text_input('OpenAI API key:', value = api_key ,type='password', on_change=clear_history)
-        guardar_clave = st.checkbox('Guardar clave')
-        st.write('Para obtener una api key, visita https://platform.openai.com/account/api-keys')
-        # si reescribes la clave
-        if api_key:
-            # cargar la clave en la variable de entorno
-            os.environ['OPENAI_API_KEY'] = api_key
-            # guardarla en el archivo .env
-        if guardar_clave:
-            with open('.env', 'w') as f:
-                f.write(f'OPENAI_API_KEY={api_key}')
+        api_key = st.text_input('OpenAI API Key:', value = api_key ,type='password', on_change=clear_history)
+        # guardar clave
+        pass_col, get_col = st.columns(2)
+        with pass_col:
+            guardar_clave = st.checkbox('Guardar clave')
+        with get_col:
+            st.write('[Obtener una API Key](https://www.youtube.com/watch?v=VpcpHjeLZSs)')
+        # estado del archivo
+        st.session_state.is_file_charged = False
         # widget para subir archivo
-        uploaded_file = st.file_uploader('Subir un archivo nuevo:', type=['m4a', 'mp3','mp4', 'mpeg', 'mpga', 'wav', 'webm'])
+        uploaded_file = st.file_uploader('Subir archivo', type=['m4a', 'mp3','mp4', 'mpeg', 'mpga', 'wav', 'webm'])
         # boton de añadir
         add_data_button = st.button('Añadir', on_click=clear_history)
         # si no ha apretado el boton de añadir pero el vector db del archivo si está cargado
-        if not add_data_button:
+    if not add_data_button:
             if  os.path.exists('db'):
                 st.success('Ya existe un archivo en memoria. Añadir otro lo sobreescribirá.')
+                st.session_state.is_file_charged = True
+            
+    st.divider()
+
+        # si reescribes la clave
+    if api_key:
+            # cargar la clave en la variable de entorno
+            os.environ['OPENAI_API_KEY'] = api_key
+            # guardarla en el archivo .env
+    if guardar_clave:
+            with open('.env', 'w') as f:
+                f.write(f'OPENAI_API_KEY={api_key}')
 
         # se sube un archivo
-        if uploaded_file and add_data_button:
+    if uploaded_file and add_data_button:
             with st.spinner('Procesando...'):
                 
                 bytes_data = uploaded_file.read()
@@ -48,13 +63,21 @@ if __name__ == "__main__":
                 with open(file_name, 'wb') as f:
                     f.write(bytes_data)
 
-                # cargar el archivo
-                print("Loading document...")
+                # transcribir el archivo
+                print("Transcribing document...")
                 transcriber = am.OpenAITranscribeAudio(api_key=api_key)
                 data = transcriber.transcribe_audio(file_path=file_name)
+                text = data['text']
+                # guardar la transcripcion
+                with open('output/transcription.txt', 'w') as f:
+                    f.write(text)
+                st.session_state.transcribed_text = text
+                #cargar el archivo
+                print("Loading document...")
+                loaded_data = tm.LoadDocument.load_document(file_path='output/transcription.txt', file_extension='.txt')
                 # separar en chunks
                 print("Chunking document...")
-                chunks = tm.ChunkData.chunk_data(data=data)
+                chunks = tm.ChunkData.chunk_data(data=loaded_data)
                 # instanciar embedding model
                 print("Creating embedding model...")
                 embedding_model = tm.OpenAIEmbeddings.create_embedding_model()
@@ -65,79 +88,84 @@ if __name__ == "__main__":
                 print("Saving vector store...")
                 st.session_state.vs = vector_store
                 st.success('Nuevo archivo cargado.')
+                st.session_state.is_file_charged = True
 
-        st.divider()
+
     
     #rol del chatbot
     system_message = 'Tu rol es'
 
     # transcripción
-    st.text_area('Transcripción: ', value=data, height=300)
+    if st.session_state.is_file_charged:
+        text = st.session_state.transcribed_text
+        st.text_area('Transcripción: ', value=text, height=300)
 
-    # obtener fecha y hora actual
-    now = datetime.datetime.now()
+        # obtener fecha y hora actual
+        now = datetime.datetime.now()
 
-    # boton de descargar transcripcion
-    st.download_button(
-    label="Descargar transcripción",
-    data=data,
-    file_name='transcripcion_'+ str(now) +'.txt',
-    mime='text/plain')
+        # boton de descargar transcripcion
+        st.download_button(
+        label="Descargar transcripción",
+        data=text,
+        file_name='transcripcion_'+ str(now) +'.txt',
+        mime='text/plain')
 
-    # query del usuario
-    query = st.text_area('Inserte pregunta sobre el audio...', height=160)
+        st.divider()
 
-    # boton de generar respuesta
-    is_button_pressed = st.button('Generar respuesta')
+        # query del usuario
+        query = st.text_area('Hacer una pregunta a la IA sobre el audio...', height=160)
 
-    # si el usuario apretó generar respuesta
-    if query and is_button_pressed:
+        # boton de generar respuesta
+        is_button_pressed = st.button('Generar respuesta')
 
-        # si los datos del archivo no han sido cargados en la sesion de Streamlit
-        if 'vs' not in st.session_state:
+        # si el usuario apretó generar respuesta
+        if query and is_button_pressed:
 
-            # instanciar embedding model
-            embedding_model = tm.OpenAIEmbeddings.create_embedding_model()
-            # crear vector store, generar y guardar embeddings
-            vector_store = tm.VectorStoreChroma.create_vector_store(chunks, embedding_model)
-            # guardar el db en la sesion de Streamlit
-            st.session_state.vs = vector_store
+            # si los datos del archivo no han sido cargados en la sesion de Streamlit
+            if 'vs' not in st.session_state:
 
-        # si los datos estan cargados
-        if 'vs' in st.session_state:
-            with st.spinner('Pensando...'):
-                
-                # recuperar vector store
-                vector_store = st.session_state.vs
-                # instanciar chatbot
-                print("Creating chatbot...")
-                chatbot = tm.OpenAIChat.create_chat_model(system_message=system_message)
-                # crear cadena de respuesta
-                print("Creating answer...")
-                answer = tm.SimpleQuestionAnswer.ask_and_get_answer(query=query, vector_store=vector_store, llm=chatbot, k=k)
-                print("Gathering sources...")
-                sources = vector_store.similarity_search(query)
+                # instanciar embedding model
+                embedding_model = tm.OpenAIEmbeddings.create_embedding_model()
+                # crear vector store, generar y guardar embeddings
+                vector_store = tm.VectorStoreChroma.create_vector_store(chunks, embedding_model)
+                # guardar el db en la sesion de Streamlit
+                st.session_state.vs = vector_store
 
-                st.text_area('Respuesta: ', value=answer, height=300)
+            # si los datos estan cargados
+            if 'vs' in st.session_state:
+                with st.spinner('Pensando...'):
+                    
+                    # recuperar vector store
+                    vector_store = st.session_state.vs
+                    # instanciar chatbot
+                    print("Creating chatbot...")
+                    chatbot = tm.OpenAIChat.create_chat_model(system_message=system_message)
+                    # crear cadena de respuesta
+                    print("Creating answer...")
+                    answer = tm.SimpleQuestionAnswer.ask_and_get_answer(query=query, vector_store=vector_store, llm=chatbot, k=3)
+                    print("Gathering sources...")
+                    sources = vector_store.similarity_search(query)
 
-                with st.expander('Fuentes: '):
-                        for source in sources:
-                            st.write(source.page_content + '\n')
+                    st.text_area('Respuesta: ', value=answer, height=300)
 
-                st.divider()
+                    with st.expander('Fuentes: '):
+                            for source in sources:
+                                st.write(source.page_content + '\n')
 
-                # si no hay historial, crearlo
-                if 'history' not in st.session_state:
-                    st.session_state.history = ''
+                    st.divider()
 
-                # pregunta actual
-                value = f'Pregunta: {query} \nRespuesta: {answer}'
+                    # si no hay historial, crearlo
+                    if 'history' not in st.session_state:
+                        st.session_state.history = ''
 
-                st.session_state.history = f'{value} \n {"-" * 20} \n {st.session_state.history}'
-                h = st.session_state.history
+                    # pregunta actual
+                    value = f'Pregunta: {query} \n\nRespuesta: {answer}'
 
-                # historial
-                with st.expander('Historial'):
-                    st.text_area(label='Historial', value=h, key='history', height=400)
+                    st.session_state.history = f'{value} \n {"-" * 20} \n {st.session_state.history}'
+                    h = st.session_state.history
+
+                    # historial
+                    with st.expander('Historial'):
+                        st.text_area(label='Historial', value=h, key='history', height=400)
 
 # para correr la app en terminal: streamlit run ./model.py
